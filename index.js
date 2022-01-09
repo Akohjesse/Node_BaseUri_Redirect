52d4885776c5cc47bfe08c5c3530fc966323ff0c
Jesse <akohgodwinjesse@gmail.com>
2022-01-09 15:24:33 +0100

    update procfile

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ app.use(express.static('src'));
 app.get('/*', async (req,res ) =>{
     let file  = {
         image: "https://warriormammoth.mypinata.cloud/ipfs/QmSWAskx3AT5DnqkKk2fBFDF8mxk6gMNL31uiHXmZdkiw1",
+        description: "Warrior mammoth nft collection inspired by the tale of the warrior tammoth",
         tokenId: req.url.substring(1),
-        name: `Warrior Mammoth #${req.url.substring(1)}`
+        name: `Mammoth #${req.url.substring(1)}`
     }
     file = JSON.stringify(file);
     fs.writeFile('src/metadata.json', file, err => {
