85bb074ad611cf48f30ce15ce2c5490ef63869c5
Jesse <akohgodwinjesse@gmail.com>
2022-01-09 14:34:05 +0100

    creation complete

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express =  require("express");
 const app = express();
 const port = process.env.PORT || 2000;
-const path = require('path')
+const path = require('path');
+const fs = require('fs')
 
 app.use(express.static('src'));
 
 
-app.get('/', (req,res ) =>{
-    res.sendFile(path.resolve(__dirname + '/src/metadata.json'));
+app.get('/*', async (req,res ) =>{
+    let file  = {
+        image: "https://warriormammoth.mypinata.cloud/ipfs/QmSWAskx3AT5DnqkKk2fBFDF8mxk6gMNL31uiHXmZdkiw1",
+        tokenId: req.url.substring(1),
+        name: `Warrior Mammoth #${req.url.substring(1)}`
+    }
+    file = JSON.stringify(file);
+    fs.writeFile('src/metadata.json', file, err => {
+       err ? err : res.sendFile(path.resolve(__dirname + "/src/metadata.json"))
+    })
+    
 })
 
 app.listen(port, ()=>{
